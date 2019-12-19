226e6f4f62d25f30186ff6651aa2541eb0acaf24
Bhumi Patel <bhumi6156@gmail.com>
2019-12-19 14:19:37 -0500

    hello world

---
+++ FirstBasicWebsite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstBasicWebsite
+//
+//  Created by Bhumi Patel on 12/19/19.
+//  Copyright © 2019 Bhumi Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello,  World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
